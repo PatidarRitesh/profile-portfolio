ad62c652fc51ead2ec19c6ac47ac585293d79c25
Ritesh Patidar <riteshpatidar2499@gmail.com>
2024-02-28 14:10:44 +0530

    error resolve

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/PatidarRitesh/PatidarRitesh.github.io.git";
+const repoURL = "https://github.com/PatidarRitesh/PatidarRitesh.githhub.io.git";
 
 ghpages.publish(
   pathname,
